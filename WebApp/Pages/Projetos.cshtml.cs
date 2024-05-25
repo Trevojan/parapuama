3d4aa00beb1d902491d04f670b5f65532996d6c6
Trevojan <erosbnk@yahoo.com>
2024-05-24 21:34:38 -0300

    Renomeando e organizando arquivos cshtml

---
+++ WebApp/Pages/Projetos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class ProjetosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
