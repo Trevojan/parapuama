bc80d1bb8249c9a89d04b20caffea382c1523ec0
Trevojan <erosbnk@yahoo.com>
2024-05-30 23:56:44 -0300

    do notebook

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -15,7 +15,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7661685152002fdc77fc012544d6dd1200f2e800")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61ce706fbe324a08fe4e758163785eb6a5164cc8")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
