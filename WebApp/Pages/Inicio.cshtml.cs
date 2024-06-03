3f7968d0848068e2f0d65600e745827c2825a488
ErosBenkert <eros_benkert@estudante.sesisenai.org.br>
2024-06-03 17:22:58 -0300

    Desenvolvimento backend - Banco de dados e funções CRUD

--- WebApp/Pages/Inicio.cshtml.cs
+++ WebApp/Pages/Inicio.cshtml.cs
@@ -15,5 +15,44 @@ namespace WebApp.Pages
         {
 
         }
+
+        private class Escopos()
+        {
+            public String TituloEscopo(string t)
+            {
+                switch (t)
+                {
+                    case "devsoft":
+                        return "a";
+                    case "palestra":
+                        return "s";
+                    case "market":
+                        return "d";
+                    case "aula":
+                        return "f";
+                    case "stats":
+                        return "g";
+                }
+                return "";
+            }
+
+            public String ConteudoEscopo(String t)
+            {
+                switch (t)
+                {
+                    case "devsoft":
+                        return "a";
+                    case "palestra":
+                        return "s";
+                    case "market":
+                        return "d";
+                    case "aula":
+                        return "f";
+                    case "stats":
+                        return "g";
+                }
+                return "";
+            }
+        }
     }
 }
