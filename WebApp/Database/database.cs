c4c5838d670ff0565dda9a9b5bc20af478e7748c
Trevojan <110431245+Trevojan@users.noreply.github.com>
2024-06-05 20:43:56 -0300

    Atualizando queries de conexão DB

--- WebApp/Database/database.cs
+++ WebApp/Database/database.cs
@@ -5,14 +5,24 @@ namespace WebApp.Database
 {
     public class Gather
     {
-        public void forward()
+        public string forward(String read)
         {
             try
             {
-                SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Users\eros_benkert\Documents\GitHub\parapuama\WebApp\Database\dbParapuama.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True");
-                string query = "SELECT * FROM INFORMATION_SCHEMA.TABLES;";
+                SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|dbParapuama.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True");
+                string query = read;
                 con.Open();
                 SqlCommand cmd = new SqlCommand(query, con);
+
+                using (var reader = cmd.ExecuteReader())
+                {if (reader.Read())
+                    {
+                        return "Found database.";
+                    }
+                }
+
+                System.Diagnostics.Debug.WriteLine(read);
+                return read;
             }
             catch (SqlException e)
             {
@@ -21,9 +31,47 @@ namespace WebApp.Database
             }
         }
 
-        public static void getTables()
+
+        public object SetCol(string c,string t)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|dbParapuama.mdf;Integrated Security=True;Connect Timeout=10;Encrypt=True");
+                string query = "SELECT COUNT(" + c + ") FROM " + t + ";";
+                con.Open();
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                var reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    reader.Close();
+                    query = "SELECT " + c + " FROM " + t + ";";
+                    cmd = new SqlCommand(query, con);
+                    reader = cmd.ExecuteReader();
+
+                    string[] val = { };
+                    int i = 0;
+
+                    foreach (var item in reader)
+                    {val[i] = (string)reader[i];
+                     i++;}
+
+                    return val;
+                }
+                else { return "No value."; }
+            }
+            catch (SqlException e)
+            {
+                throw;
+            }
+        }
+
+        public void SetTables()
         {
             dynamic tbCargos = new ExpandoObject();
+                    tbCargos.NewProp = SetCol("idCargo","tbCargos");
+                    tbCargos.NewProp = SetCol("colNome", "tbCargos");
+                    tbCargos.NewProp = SetCol("colCor", "tbCargos");
             dynamic tbDirecionado = new ExpandoObject();
             dynamic tbEncarregado = new ExpandoObject();
             dynamic tbEnquadrado = new ExpandoObject();
