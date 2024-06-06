b1f60027a73c8d20727f22f2bf433f5a2a3946a1
ErosBenkert <eros_benkert@estudante.sesisenai.org.br>
2024-06-06 17:21:10 -0300

    commitado pós-caixapreta

--- WebApp/Database/database.cs
+++ WebApp/Database/database.cs
@@ -31,34 +31,28 @@ namespace WebApp.Database
             }
         }
 
-
-        public object SetCol(string c,string t)
+        public object SetCol(string c, string t)
         {
             try
             {
-                SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|dbParapuama.mdf;Integrated Security=True;Connect Timeout=10;Encrypt=True");
-                string query = "SELECT COUNT(" + c + ") FROM " + t + ";";
-                con.Open();
-                SqlCommand cmd = new SqlCommand(query, con);
+                string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|dbParapuama.mdf;Integrated Security=True;Connect Timeout=10;Encrypt=True";
+                string query = $"SELECT COUNT({c}) FROM {t};";
 
-                var reader = cmd.ExecuteReader();
-                if (reader.Read())
+                using (SqlConnection con = new SqlConnection(connectionString))
                 {
-                    reader.Close();
-                    query = "SELECT " + c + " FROM " + t + ";";
-                    cmd = new SqlCommand(query, con);
-                    reader = cmd.ExecuteReader();
-
-                    string[] val = { };
-                    int i = 0;
-
-                    foreach (var item in reader)
-                    {val[i] = (string)reader[i];
-                     i++;}
-
-                    return val;
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        var reader = cmd.ExecuteReader();
+                        int count = 0;
+                        while (reader.Read())
+                        {
+                            count = (int)reader[0];
+                        }
+                        Console.WriteLine($"Count: {count}");
+                        return count;
+                    }
                 }
-                else { return "No value."; }
             }
             catch (SqlException e)
             {
@@ -69,24 +63,97 @@ namespace WebApp.Database
         public void SetTables()
         {
             dynamic tbCargos = new ExpandoObject();
-                    tbCargos.NewProp = SetCol("idCargo","tbCargos");
-                    tbCargos.NewProp = SetCol("colNome", "tbCargos");
-                    tbCargos.NewProp = SetCol("colCor", "tbCargos");
+                    tbCargos.id = SetCol("idCargo", "tbCargos");
+                    tbCargos.nome = SetCol("colNome", "tbCargos");
+                    tbCargos.cor = SetCol("colCor", "tbCargos");
+
             dynamic tbDirecionado = new ExpandoObject();
+                    tbDirecionado.projeto = SetCol("idProjetoO", "tbDirecionado");
+                    tbDirecionado.id = SetCol("idDirecionado", "tbDirecionado");
+                    tbDirecionado.objetivo = SetCol("idObjetivo", "tbDirecionado");
+
             dynamic tbEncarregado = new ExpandoObject();
+                    tbEncarregado.projeto = SetCol("idProjetoC", "tbEncarregado");
+                    tbEncarregado.id = SetCol("idEncarregado", "tbEncarregado");
+                    tbEncarregado.cargo = SetCol("idCargo", "tbEncarregado");
+                    tbEncarregado.usuario = SetCol("idUsuario", "tbEncarregado");
+            
             dynamic tbEnquadrado = new ExpandoObject();
+                    tbEnquadrado.projeto = SetCol("idProjetoQ", "tbEnquadrado");
+                    tbEnquadrado.id = SetCol("idEnquadrado", "tbEnquadrado");
+                    tbEnquadrado.quadro = SetCol("idQuadro", "tbEnquadrado");
+            
             dynamic tbFerramentas = new ExpandoObject();
+                    tbFerramentas.id = SetCol("idFerramenta", "tbFerramentas");
+                    tbFerramentas.nome = SetCol("colNome", "tbFerramentas");
+                    tbFerramentas.utilidade = SetCol("colUtilidade", "tbFerramentas");
+                    tbFerramentas.funcao = SetCol("colFuncao", "tbFerramentas");
+                    tbFerramentas.estado = SetCol("colEstado", "tbFerramentas");
+            
             dynamic tbGrupos = new ExpandoObject();
+                    tbGrupos.id = SetCol("idGrupo", "tbGrupos");
+                    tbGrupos.nome = SetCol("colNome", "tbGrupos");
+                    tbGrupos.descricao = SetCol("colDescricao", "tbGrupos");
+                    tbGrupos.limite = SetCol("colLimite", "tbGrupos");
+                    tbGrupos.data = SetCol("colDataCriacao", "tbGrupos");
+            
             dynamic tbIdealizacao = new ExpandoObject();
+                    tbIdealizacao.id = SetCol("idIdeia", "tbIdealizacao");
+                    tbIdealizacao.descricao = SetCol("colDescricao", "tbIdealizacao");
+                    tbIdealizacao.estado = SetCol("colEstado", "tbIdealizacao");
+                    tbIdealizacao.data = SetCol("colDataCriacao", "tbIdealizacao");
+            
             dynamic tbIdealizado = new ExpandoObject();
+                    tbIdealizado.projeto = SetCol("idProjetoI", "tbIdealizado");
+                    tbIdealizado.id = SetCol("idIdealizado", "tbIdealizado");
+                    tbIdealizado.ideia = SetCol("idIdeias", "tbIdealizado");
+            
             dynamic tbObjetivos = new ExpandoObject();
+                    tbObjetivos.id = SetCol("idObjetivo", "tbObjetivos");
+                    tbObjetivos.nome = SetCol("colNome", "tbObjetivos");
+                    tbObjetivos.descricao = SetCol("colDescricao", "tbObjetivos");
+                    tbObjetivos.estado = SetCol("colEstado", "tbObjetivos");
+            
             dynamic tbParticipantes = new ExpandoObject();
+                    tbParticipantes.projeto = SetCol("idProjetoP", "tbParticipantes");
+                    tbParticipantes.id = SetCol("idParticipante", "tbParticipantes");
+                    tbParticipantes.usuario = SetCol("idUsuario", "tbParticipantes");
+            
             dynamic tbProjetos = new ExpandoObject();
+                    tbProjetos.projeto = SetCol("idProjeto", "tbProjetos");
+                    tbProjetos.nome = SetCol("colNome", "tbProjetos");
+                    tbProjetos.descricao = SetCol("colDescricao", "tbProjetos");
+            
             dynamic tbQuadros = new ExpandoObject();
+                    tbQuadros.id = SetCol("idQuadro", "tbQuadros");
+                    tbQuadros.nome = SetCol("colNome", "tbQuadros");
+            
             dynamic tbSuper = new ExpandoObject();
+                    tbSuper.id = SetCol("idSuper", "tbSuper");
+                    tbSuper.participantes = SetCol("idParticipantes", "tbSuper");
+                    tbSuper.ferramentas = SetCol("idFerramentas", "tbSuper");
+                    tbSuper.quadros = SetCol("idQuadros", "tbSuper");
+                    tbSuper.objetivos = SetCol("idObjetivos", "tbSuper");
+                    tbSuper.ideias = SetCol("idIdealizado", "tbSuper");
+            
             dynamic tbTarefas = new ExpandoObject();
-            dynamic tbUsuarios = new ExpandoObject();
+                    tbTarefas.id = SetCol("idTarefa", "tbTarefas");
+                    tbTarefas.nome = SetCol("colNome", "tbTarefas");
+                    tbTarefas.tipo = SetCol("colTipo", "tbTarefas");
+                    tbTarefas.estado = SetCol("colEstado", "tbTarefas");
+            
             dynamic tbUtilizando = new ExpandoObject();
+                    tbUtilizando.projeto = SetCol("idProjetoF", "tbUtilizando");
+                    tbUtilizando.idealizado = SetCol("idIdealizado", "tbUtilizando");
+                    tbUtilizando.ferramenta = SetCol("idFerramenta", "tbUtilizando");
+
+            dynamic tbUsuarios = new ExpandoObject();
+                    tbUsuarios.id = SetCol("idUsuario", "tbUsuarios");
+                    tbUsuarios.login = SetCol("colLogin", "tbUsuarios");
+                    tbUsuarios.senha = SetCol("colSenha", "tbUsuarios");
+                    tbUsuarios.email = SetCol("colEmail", "tbUsuarios");
+                    tbUsuarios.apelido = SetCol("colApelido", "tbUsuarios");
+                    tbUsuarios.biografia = SetCol("colBiografia", "tbUsuarios");
         }
     }
 }
