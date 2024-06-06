b97074606931674a87d6c061634f3719be52725e
Trevojan <110431245+Trevojan@users.noreply.github.com>
2024-06-06 20:15:56 -0300

    Inicio da camada de usuários e implementação correta do MVC

--- WebApp/Database/read.cs
+++ WebApp/Database/read.cs
@@ -1,6 +1,12 @@
-﻿namespace WebApp.Database
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+
+
+namespace WebApp.Database
 {
     public class Read
     {
+        public 
     }
 }
--- WebApp/Pages/Login.cshtml.cs
+++ WebApp/Pages/Login.cshtml.cs
@@ -1,12 +1,51 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 
 namespace WebApp.Pages
 {
     public class LoginModel : PageModel
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public void OnGet()
+        public LoginModel(IHttpContextAccessor httpContextAccessor)
+        { _httpContextAccessor = httpContextAccessor; }
+
+        List<int> Session = new List<int>();
+        public bool GetLogin(string l, string p)
         {
+            try
+            {
+                string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|dbParapuama.mdf;Integrated Security=True;Connect Timeout=10;Encrypt=True";
+                string query = $"SELECT idUsuario FROM tbUsuarios WHERE {l} = colLogin AND {p} = colSenha";
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    var reader = cmd.ExecuteReader();
+
+                    Session.Add(reader.GetInt32(0));
+
+                    if (reader.Read())
+                    { return true; }
+                    return false;
+                }
+            }
+            catch (SqlException e)
+            {
+                System.Diagnostics.Debug.WriteLine("Erro: " + e);
+                throw;
+            }
+        }
+
+        public object CheckLogin(int id)
+        {
+            if (Session.Contains(id))
+            {
+
+            }
+
+            return 0;
         }
     }
 }
+
